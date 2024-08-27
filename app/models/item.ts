4c64bd297c50ae8f9b7a6cfacfd5f8c8851f7180
reiwa <reiwa@outlook.com.vn>
2024-08-27 16:08:07 +0900

    refactor

--- app/models/item.ts
+++ app/models/item.ts
@@ -1,9 +1,9 @@
 import { union } from "valibot"
-import { vItemStory } from "~/models/item-ask"
 import { vItemComment } from "~/models/item-comment"
 import { vItemJob } from "~/models/item-job"
 import { vItemPoll } from "~/models/item-poll"
 import { vItemPollopt } from "~/models/item-pollopt"
+import { vItemStory } from "~/models/item-story"
 
 export const vItem = union([
   vItemComment,
